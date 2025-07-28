b1684678c218ef71e608fc91c3068f2ee2e4c233
malkiel <malkiel@MacBook-Pro-malkiel---backup.local>
2025-07-28 21:10:02 +0300

    update 2

--- form/js/form-validation.js
+++ form/js/form-validation.js
@@ -1,11 +1,11 @@
-// form/js/form-validation.js - ולידציה ובדיקת שדות
-$(document).ready(function() {
-    // פונקציה לבדיקת שלמות הטופס
-    function checkFormCompleteness() {
-        if (formConfig.isViewOnly) return true;
+// // form/js/form-validation.js - ולידציה ובדיקת שדות
+// $(document).ready(function() {
+//     // פונקציה לבדיקת שלמות הטופס
+//     function checkFormCompleteness() {
+//         if (formConfig.isViewOnly) return true;
 
-        let missingFields = [];
-        let isComplete = true;
+//         let missingFields = [];
+//         let isComplete = true;
 
         // // עבור על כל שדות החובה
         // $('[data-required="true"]:not(:disabled)').each(function() {
