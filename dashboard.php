37470da8720e93ef379f8b19f1a65b8043d72270
Malkiel Ben-Ezra <052840@gmail.com>
2025-08-04 20:45:13 +0300

    הרשאות ורכישות

--- dashboard.php
+++ dashboard.php
@@ -4,7 +4,7 @@ require_once 'config.php';
 
 // בדיקת התחברות
 if (!isset($_SESSION['user_id'])) {
-    header("Location: " . SITE_URL . "/auth/login.php");
+    header("Location: " . SITE_URL . "auth/login.php");
     exit;
 }
 
