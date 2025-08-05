a17699de2b4ede0e5db40ede96cfbaef53ecba59
Malkiel Ben-Ezra <052840@gmail.com>
2025-08-05 15:47:36 +0300

    dashbord1

--- includes/dashboard.php
+++ includes/dashboard.php
@@ -163,6 +163,7 @@ $recentActivity = array_slice($recentActivity, 0, 10);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link href="../css/dashboard-styles-optimized.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
     <?php require_once 'nav.php'; ?>
