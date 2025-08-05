2a71b955c44b7e2f0d80ed4f45690ff8be52f516
Malkiel Ben-Ezra <052840@gmail.com>
2025-08-05 14:52:58 +0300

    dashbord1

--- includes/dashboard.php
+++ includes/dashboard.php
@@ -217,7 +217,7 @@ $recentActivity = array_slice($recentActivity, 0, 10);
                                     <span class="counter-label">היום</span>
                                 </div>
                             </div>
-                            <a href="../<?= FORM_URL ?>" class="btn btn-primary action-btn mt-3 w-100" onclick="event.stopPropagation();">
+                            <a href="../<?= FORM_DECEASED_URL ?>" class="btn btn-primary action-btn mt-3 w-100" onclick="event.stopPropagation();">
                                 <i class="fas fa-plus"></i> טופס חדש
                             </a>
                         </div>
