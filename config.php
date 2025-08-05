668027a6a9d3f694aa98241e45e5598592272df6
Malkiel Ben-Ezra <052840@gmail.com>
2025-08-05 17:26:36 +0300

    dashbord1

--- config.php
+++ config.php
@@ -64,10 +64,6 @@ define('LOGIN_URL', 'auth/login.php');
 define('LOGOUT_URL', 'auth/logout.php');
 define('LOGIN_SITE_URL', SITE_URL .  'auth/login.php');
 
-// define('FORM_URL', 'form/index.php');
-// define('FORM_DECEASED_URL', 'form/index_deceased.php');
-// define('FORM_PURCHASE_URL', 'form/index_purchase.php');
-
 define('FORM_URL', 'form/index_deceased.php');
 define('FORM_DECEASED_URL', 'form/index_deceased.php');
 define('FORM_PURCHASE_URL', 'form/index_purchase.php');
