40747b32192fc67637198b47608822659e3467b8
Malkiel Ben-Ezra <052840@gmail.com>
2025-08-05 21:28:46 +0300

    dashbord2

--- includes/dashboard.php
+++ includes/dashboard.php
@@ -236,7 +236,8 @@ $recentActivity = array_slice($recentActivity, 0, 10);
                             <h3 class="mt-3">טפסי נפטרים</h3>
                             <p class="text-muted mb-0 d-none d-md-block">ניהול רישום נפטרים וקבורה</p>
                         </div>
-                        <div class="text-center text-md-end w-100 w-md-auto" style="border: 1px red solid">
+                        style="border: 1px red solid"
+                        <div class="text-center text-md-end w-100 w-md-auto">
                             <div class="counter-section">
                                 <div class="counter-item">
                                     <span class="counter-value text-primary"><?= number_format($stats['deceased']['total']) ?></span>
