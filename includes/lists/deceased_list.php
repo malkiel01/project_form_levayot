d5c09d2b55588d4524489f02524f8a754eede369
Malkiel Ben-Ezra <052840@gmail.com>
2025-08-11 18:04:48 +0300

    cemeteries7-3

--- includes/lists/deceased_list.php
+++ includes/lists/deceased_list.php
@@ -223,7 +223,7 @@ function exportDeceasedList() {
                         </thead>
                         <tbody>
                             <?php foreach ($results as $row): ?>
-                                <tr onclick="window.location='./view_deceased_form.php?id=<?= $row['form_uuid'] ?>'">
+                                <tr onclick="window.location='../../form/include/view_deceased_form.php?id=<?= $row['form_uuid'] ?>'">
                                     <td><?= substr($row['form_uuid'], 0, 8) ?>...</td>
                                     <td><strong><?= htmlspecialchars(trim($row['deceased_first_name'] . ' ' . $row['deceased_last_name'])) ?></strong></td>
                                     <td><?= htmlspecialchars($row['identification_number'] ?? '-') ?></td>
@@ -248,7 +248,7 @@ function exportDeceasedList() {
                                     <td><?= date('d/m/Y H:i', strtotime($row['created_at'])) ?></td>
                                     <td onclick="event.stopPropagation()">
                                         <div class="btn-group btn-group-sm">
-                                            <a href="./view_deceased_form.php?id=<?= $row['form_uuid'] ?>" 
+                                            <a href="../../form/include/view_deceased_form.php?id=<?= $row['form_uuid'] ?>" 
                                                class="btn btn-info" title="צפייה">
                                                 <i class="fas fa-eye"></i>
                                             </a>
